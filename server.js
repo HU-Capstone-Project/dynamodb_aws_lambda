c8313b932d920613ce2a9ecbb0374a648cc4a07a
hsntrq <mohdhasantariq@gmail.com>
2022-11-20 12:51:30 +0500

    scan table api

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const {
   DynamoDBClient,
   BatchWriteItemCommand,
+  ScanCommand,
 } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 const { v4: uuidv4 } = require("uuid");
 
 const client = new DynamoDBClient({ region: "me-central-1" });
 
 exports.bulkdynamodb = async (event) => {
-  let response;
   switch (event.path) {
     case "/insert/":
+      if (!event.body)
+        return {
+          statusCode: 403,
+          body: JSON.stringify({ message: "Forbidden" }),
+        };
       const data = event.body.split("\n");
       const { nodeid, studyid } = data[0].split(",");
       const headers = data[1].split(",");
@@ -23,7 +29,7 @@ exports.bulkdynamodb = async (event) => {
           const obj = {
             PutRequest: {
               Item: {
-                id: { "S": uuidv4() },
+                id: { S: uuidv4() },
               },
             },
           };
@@ -31,7 +37,7 @@ exports.bulkdynamodb = async (event) => {
           const vals = data[i].split(",");
 
           for (let j = 0; j < headers.length; j++) {
-            obj.PutRequest.Item[headers[j]] = { "N": vals[j] };
+            obj.PutRequest.Item[headers[j]] = { N: vals[j] };
           }
           request.push(obj);
 
@@ -56,12 +62,12 @@ exports.bulkdynamodb = async (event) => {
           const createResult = await client.send(
             new BatchWriteItemCommand(params)
           );
-          result.push([createResult, params]);
+          result.push(createResult);
           request.length = 0;
         }
       } catch (e) {
         console.error(e);
-        result.push([e,params]);
+        result.push(e);
       }
 
       response = {
@@ -69,9 +75,27 @@ exports.bulkdynamodb = async (event) => {
         body: JSON.stringify(result),
       };
       return response;
+    case "/samples/":
+      const response = { statusCode: 200 };
+      try {
+        const { Items } = await client.send(
+          new ScanCommand({ TableName: profile_sample })
+        );
+        response.body = JSON.stringify(Items.map((item) => unmarshall(item)));
+      } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+          message: "Failed to retrieve posts.",
+          errorMsg: e.message,
+          errorStack: e.stack,
+        });
+      }
+      return response;
+
     default:
       return {
-        statusCode: 400,
+        statusCode: 404,
         body: JSON.stringify({ message: "Not Found" }),
       };
   }
