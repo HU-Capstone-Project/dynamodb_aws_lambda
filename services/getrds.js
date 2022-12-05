d5ae31671bfd480ca4f1ef2a7c84463819ff2f73
hsntrq <mohdhasantariq@gmail.com>
2022-12-05 10:19:53 +0500

    rds services added

---
+++ services/getrds.js
@@ -0,0 +1,54 @@
+module.exports.getCars = async (client, id) => {
+  const response = { statusCode: 200 };
+  try {
+    const q = `select * from car ${id ? `where id=${id}` : ""}`;
+    const res = await client.query(q);
+    response.body = JSON.stringify(res.rows);
+  } catch (e) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to retrieve cars.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
+
+module.exports.getSurveyors = async (client, id) => {
+  const response = { statusCode: 200 };
+  try {
+    const q = `select * from surveyor join car on car.id=car_id ${
+      id ? `where surveyor.id=${id}` : ""
+    } `;
+    const res = await client.query(q);
+    response.body = JSON.stringify(res.rows);
+  } catch (e) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to retrieve surveyors.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
+
+module.exports.getProfiles = async (client, id) => {
+  const response = { statusCode: 200 };
+  try {
+    const q = `select * from profile join surveyor on surveyor.id=surveyor_id ${
+      id ? `where surveyor.id=${id}` : ""
+    } `;
+    const res = await client.query(q);
+    response.body = JSON.stringify(res.rows);
+  } catch (e) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to retrieve profiles.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
--- services/index.js
+++ services/index.js
@@ -1,7 +1,11 @@
 const getSamples = require("./getSamples");
 const insertSample = require("./insertSample");
+const { getCars, getProfiles, getSurveyors } = require("./getrds");
 
 module.exports = {
   insertSample,
   getSamples,
+  getCars,
+  getProfiles,
+  getSurveyors,
 };
